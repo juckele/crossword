3c34045cb3d58590c248e7b23174b57a1bf31741
Monica Zhou <mzhou.college@gmail.com>
2015-06-19 23:36:58 -0400

    Monica's first commit

--- src/com/johnuckele/puzzle/crossword/HelloGradle.java
+++ src/com/johnuckele/puzzle/crossword/HelloGradle.java
@@ -3,5 +3,6 @@ package com.johnuckele.puzzle.crossword;
 public class HelloGradle {
 	public static void main(String[] args) {
 		System.out.println("Hello Gradle");
+		System.out.println("Hello John and Vidie!");
 	}
 }
